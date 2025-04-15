677d3feb4c0bc4e7cb133c6e0fd115299a55a0ea
Bence Szabo <bence@Bences-MacBook-Pro.local>
2025-04-15 22:01:36 +0200

    Fix: add Team component to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,24 @@
 'use client';
-import Team from '../components/Team';
-import Offerings from '../components/Offerings';
+
 import Hero from '../components/Hero';
 import Overview from '../components/Overview';
 import Technology from '../components/Technology';
 import Investment from '../components/Investment';
+import Offerings from '../components/Offerings';
+import Team from '../components/Team';
+import ContactForm from '../components/ContactForm';
 
 export default function Home() {
   return (
-    <main className="bg-white text-gray-900">
+    <div className="min-h-screen bg-white text-gray-900">
       <Hero />
       <Overview />
       <Technology />
       <Investment />
       <Offerings />
-
-    </main>
+      <Team />
+      <ContactForm />
+    </div>
   );
 }
+
