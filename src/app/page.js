d285b030db2994c3e5d2f41d5ce95012087c2372
Bence Szabo <bence@Bences-MacBook-Pro.local>
2025-04-15 21:27:55 +0200

    Fix broken import paths to relative format

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 'use client';
 
 import Hero from '../components/Hero';
+// import Overview from '../components/Overview';
+// import Technology from '../components/Technology';
+// import Investment from '../components/Investment';
+// import Offerings from '../components/Offerings';
+// import Team from '../components/Team';
+// import ContactForm from '../components/ContactForm';
 
 export default function Home() {
   return (
