72fe3ff4469feb30dcfc4012b37b387cc31d0605
Bence Szabo <bence@Bences-MacBook-Pro.local>
2025-04-15 21:06:17 +0200

    Temp fix: use only Hero.js to pass build

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Hero() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <h1 className="text-4xl font-bold">Greenflow is ALIVE 🌱</h1>
+    </div>
+  );
+}
+
